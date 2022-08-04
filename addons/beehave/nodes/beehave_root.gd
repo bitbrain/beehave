e289eed41fa636c36b16655d5c0c03e6776a5e48
Dono <dono.orhan@gmail.com>
2022-08-04 18:55:32 +0200

    Allow custom actor on root node

--- addons/beehave/nodes/beehave_root.gd
+++ addons/beehave/nodes/beehave_root.gd
@@ -9,6 +9,10 @@ const RUNNING = 2
 
 @export var enabled : bool = true
 
+@export_node_path var actor_node_path : NodePath
+
+var actor : Node
+
 @onready var blackboard = Blackboard.new()
 
 func _ready():
@@ -16,26 +20,31 @@ func _ready():
 		push_error("Beehave error: Root should have one child")
 		disable()
 		return
+
+	actor = get_parent()
+	if actor_node_path:
+		actor = get_node(actor_node_path)
+
 	set_physics_process(enabled)
 
 func _physics_process(delta):
 	blackboard.set("delta", delta)
 
-	var status = self.get_child(0).tick(get_parent(), blackboard)
-	
+	var status = self.get_child(0).tick(actor, blackboard)
+
 	if status != RUNNING:
 		blackboard.set("running_action", null)
-	
+
 func get_running_action():
 	if blackboard.has("running_action"):
 		return blackboard.get("running_action")
 	return null
-		
+
 func get_last_condition():
 	if blackboard.has("last_condition"):
 		return blackboard.get("last_condition")
 	return null
-	
+
 func get_last_condition_status():
 	if blackboard.has("last_condition_status"):
 		var status = blackboard.get("last_condition_status")
