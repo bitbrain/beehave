ef2948336ec41e2b401059ddab12d8a192642552
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-01-26 23:19:47 +0000

    :bug: set correctly last condition on decorators

--- addons/beehave/nodes/decorators/failer.gd
+++ addons/beehave/nodes/decorators/failer.gd
@@ -7,6 +7,9 @@ class_name AlwaysFailDecorator extends Decorator
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	for c in get_children():
 		var response = c.tick(actor, blackboard)
+		if c is ConditionLeaf:
+			blackboard.set_value("last_condition", c, str(actor.get_instance_id()))
+			blackboard.set_value("last_condition_status", response, str(actor.get_instance_id()))
 		if response == RUNNING:
 			if c is ActionLeaf:
 				blackboard.set_value("running_action", c, str(actor.get_instance_id()))
--- addons/beehave/nodes/decorators/limiter.gd
+++ addons/beehave/nodes/decorators/limiter.gd
@@ -18,6 +18,11 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 		blackboard.set_value(cache_key, current_count + 1, str(actor.get_instance_id()))
 		var child = self.get_child(0)
 		var response = child.tick(actor, blackboard)
+		
+		if child is ConditionLeaf:
+			blackboard.set_value("last_condition", child, str(actor.get_instance_id()))
+			blackboard.set_value("last_condition_status", response, str(actor.get_instance_id()))
+		
 		if child is ActionLeaf and response == RUNNING:
 			blackboard.set_value("running_action", child, str(actor.get_instance_id()))
 		return response
