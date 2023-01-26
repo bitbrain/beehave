f17c2ee29cb27e37bf9c8b0c57af6f11be0ea6ab
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-01-26 23:30:25 +0000

    add additional tests

--- test/beehave_tree_test.gd
+++ test/beehave_tree_test.gd
@@ -16,6 +16,7 @@ func test_nothing_running_before_first_tick() -> void:
 	var runner := scene_runner(scene)
 	assert_that(scene.beehave_tree.get_running_action()).is_null()
 	assert_that(scene.beehave_tree.get_last_condition()).is_null()
+	assert_that(scene.beehave_tree.get_last_condition_status()).is_equal("")
 	
 func test_get_last_condition() -> void:
 	var scene = create_scene()
@@ -23,3 +24,18 @@ func test_get_last_condition() -> void:
 	scene.beehave_tree._physics_process(1.0)
 	assert_that(scene.beehave_tree.get_running_action()).is_null()
 	assert_that(scene.beehave_tree.get_last_condition()).is_not_null()
+	assert_that(scene.beehave_tree.get_last_condition_status()).is_equal("FAILURE")
+	
+func test_disabled() -> void:
+	var scene = create_scene()
+	var runner := scene_runner(scene)
+	scene.beehave_tree.disable()
+	assert_bool(scene.beehave_tree.enabled).is_false()
+	
+
+func test_reenabled() -> void:
+	var scene = create_scene()
+	var runner := scene_runner(scene)
+	scene.beehave_tree.disable()
+	scene.beehave_tree.enable()
+	assert_bool(scene.beehave_tree.enabled).is_true()
