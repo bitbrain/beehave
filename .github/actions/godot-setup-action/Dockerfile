6d9826cd0db7a8522bc8b4b578fb2a819d398f52
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-01-27 23:44:03 +0000

    add action

---
+++ .github/actions/godot-setup-action/Dockerfile
@@ -0,0 +1,5 @@
+
+FROM alpine:3.14
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ .github/actions/godot-setup-action/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GODOT_URL=http://downloads.tuxfamily.org/godotengine/$INPUT_VERSION
+GODOT_BINARY=Godot_v$INPUT_VERSION-${INPUT_PRERELEASE_VERSION}_linux.x86_64
+
+
+
+if  [[ -z "$INPUT_PRERELEASE_VERSION" ]]
+then
+    echo "Downloading  $GODOT_URL/$GODOT_BINARY.zip..."
+    curl $GODOT_URL/$GODOT_BINARY.zip > $GODOT_BINARY.zip
+else
+    echo "Downloading $GODOT_URL/$INPUT_PRERELEASE_VERSION/$GODOT_BINARY.zip..."
+    curl $GODOT_URL/$INPUT_PRERELEASE_VERSION/$GODOT_BINARY.zip > $GODOT_BINARY.zip
+fi
+
+unzip $GODOT_BINARY.zip && rm $GODOT_BINARY.zip
+
+ls -lsa
