6ff8d6a0a622fe4eb00745e7b98648656f996210
Miguel Gonzalez Sanchez <m.gonzalez.sanchez.1990@gmail.com>
2023-01-28 16:51:43 +0000

    :test_tube: add advanced blackboard tests

--- test/beehave_tree_test.gd
+++ test/beehave_tree_test.gd
@@ -21,16 +21,19 @@ func test_nothing_running_before_first_tick() -> void:
 func test_get_last_condition() -> void:
 	var scene = create_scene()
 	var runner := scene_runner(scene)
-	scene.beehave_tree._physics_process(1.0)
+	await runner.simulate_frames(10)
 	assert_that(scene.beehave_tree.get_running_action()).is_null()
 	assert_that(scene.beehave_tree.get_last_condition()).is_not_null()
-	assert_that(scene.beehave_tree.get_last_condition_status()).is_equal("FAILURE")
+	assert_that(scene.beehave_tree.get_last_condition_status()).is_equal('SUCCESS')
 	
 func test_disabled() -> void:
 	var scene = create_scene()
 	var runner := scene_runner(scene)
 	scene.beehave_tree.disable()
+	await runner.simulate_frames(10)
 	assert_bool(scene.beehave_tree.enabled).is_false()
+	assert_that(scene.beehave_tree.get_running_action()).is_null()
+	assert_that(scene.beehave_tree.get_last_condition()).is_null()
 	
 
 func test_reenabled() -> void:
@@ -38,4 +41,6 @@ func test_reenabled() -> void:
 	var runner := scene_runner(scene)
 	scene.beehave_tree.disable()
 	scene.beehave_tree.enable()
+	await runner.simulate_frames(10)
 	assert_bool(scene.beehave_tree.enabled).is_true()
+	assert_that(scene.beehave_tree.get_last_condition()).is_not_null()
