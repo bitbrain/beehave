6a3957f62cb418f37c33d80ac993be4b5e716508
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-01-28 09:42:40 +0000

    another attempt

--- runtest.sh
+++ runtest.sh
@@ -8,7 +8,7 @@ fi
 
 # we not use no-window because of issue https://github.com/godotengine/godot/issues/55379
 #$GODOT_BIN --no-window -s -d ./addons/gdUnit4/bin/GdUnitCmdTool.gd $*
-$GODOT_BIN --headless -e -s -d addons/gdUnit4/bin/GdUnitCmdTool.gd $*
+$GODOT_BIN --headless --quit -e --path . -s addons/gdUnit4/bin/GdUnitCmdTool.gd $*
 exit_code=$?
-$GODOT_BIN --headless -e -s -d addons/gdUnit4/bin/GdUnitCopyLog.gd $* > /dev/null
+$GODOT_BIN --headless --quit -e --path . -s addons/gdUnit4/bin/GdUnitCopyLog.gd $* > /dev/null
 exit $exit_code
