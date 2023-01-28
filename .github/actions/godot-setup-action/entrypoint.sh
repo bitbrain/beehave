fa8978122474dbe1abec6a09f8b883d190c02d5b
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-01-28 00:02:00 +0000

    progress

--- .github/actions/godot-setup-action/entrypoint.sh
+++ .github/actions/godot-setup-action/entrypoint.sh
@@ -3,8 +3,6 @@
 GODOT_URL=http://downloads.tuxfamily.org/godotengine/$INPUT_VERSION
 GODOT_BINARY=Godot_v$INPUT_VERSION-${INPUT_PRERELEASE_VERSION}_linux.x86_64
 
-
-
 if  [[ -z "$INPUT_PRERELEASE_VERSION" ]]
 then
     echo "Downloading  $GODOT_URL/$GODOT_BINARY.zip..."
@@ -16,4 +14,4 @@ fi
 
 unzip $GODOT_BINARY.zip && rm $GODOT_BINARY.zip
 
-ls -lsa
+echo "::set-output godot_binary=$GODOT_BINARY"
