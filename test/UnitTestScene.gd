474ac0a56610c8451c7ea810d7ba795db2122dba
miguel <miguel-gonzalez@gmx.de>
2023-01-28 02:01:09 +0000

    :test_tube: interrupt unit test (#113)

--- test/UnitTestScene.gd
+++ test/UnitTestScene.gd
@@ -3,3 +3,4 @@ class_name UnitTestScene extends Node2D
 @onready var beehave_tree:BeehaveTree = %BeehaveTree
 @onready var blackboard:Blackboard = %Blackboard
 @onready var test_node = $Node2D
+@onready var count_up_action = %CountUpAction
