5bb3f31e29ece4ea45cd7743f17e3925ac734e50
Miguel Gonzalez Sanchez <m.gonzalez.sanchez.1990@gmail.com>
2023-02-13 14:38:55 +0000

    :bug: do only unregister monitor when present

--- addons/beehave/nodes/decorators/decorator.gd
+++ addons/beehave/nodes/decorators/decorator.gd
@@ -10,7 +10,7 @@ func _ready():
 		return
 	
 	if self.get_child_count() != 1:
-		push_error("Beehave Error: Decorator %s should have only one child (NodePath: %s)" % [self.name, self.get_path()])
+		push_warning("Beehave Error: Decorator %s should have only one child (NodePath: %s)" % [self.name, self.get_path()])
 
 
 func _get_configuration_warnings() -> PackedStringArray:
