34ed3516c759350314bb4ade9f3db8a70a6851a7
Dabbles in too many things <mitch.zais@gmail.com>
2023-09-23 11:58:01 -0700

    Update addons/beehave/debug/graph_edit.gd
    Co-authored-by: miguel <miguel-gonzalez@gmx.de>

--- addons/beehave/debug/graph_edit.gd
+++ addons/beehave/debug/graph_edit.gd
@@ -47,7 +47,7 @@ func _ready() -> void:
 	layout_button.flat = true
 	layout_button.focus_mode = Control.FOCUS_NONE
 	layout_button.pressed.connect(func(): horizontal_layout = not horizontal_layout)
-	get_menu_hbox().add_child(layout_button)
+	get_box().add_child(layout_button)
 	_update_layout_button()
 
 
