319eb1e4709cefd5ed30bf9aa861b7ce8ad69cdf
bitbrain <miguel-gonzalez@gmx.de>
2023-11-16 23:59:40 +0000

    tweak test timings

--- test/nodes/decorators/time_limiter_test.gd
+++ test/nodes/decorators/time_limiter_test.gd
@@ -20,22 +20,22 @@ var runner:GdUnitSceneRunner
 
 func before_test() -> void:
 	tree = auto_free(load(__tree).new())
-	action = auto_free(load(__action).new())
-	time_limiter = auto_free(load(__source).new())
-	runner = scene_runner(tree)
-	
 	actor = auto_free(Node2D.new())
 	blackboard = auto_free(load(__blackboard).new())
 	
-	tree.add_child(time_limiter)
-	time_limiter.add_child(action)
-	
 	tree.actor = actor
 	tree.blackboard = blackboard
+	action = auto_free(load(__action).new())
+	time_limiter = auto_free(load(__source).new())
+	
+	time_limiter.add_child(action)
+	tree.add_child(time_limiter)
+	
+	runner = scene_runner(tree)
 
 
 func test_return_failure_when_child_exceeds_time_limiter() -> void:
-	time_limiter.wait_time = 0.3
+	time_limiter.wait_time = 0.1
 	action.status = BeehaveNode.RUNNING
 	await runner.simulate_frames(1, 10)
 	assert_that(tree.tick()).is_equal(BeehaveNode.RUNNING)
@@ -44,7 +44,7 @@ func test_return_failure_when_child_exceeds_time_limiter() -> void:
 
 
 func test_reset_when_child_finishes() -> void:
-	time_limiter.wait_time = 1.0
+	time_limiter.wait_time = 0.5
 	action.status = BeehaveNode.RUNNING
 	await runner.simulate_frames(1, 10)
 	assert_that(tree.tick()).is_equal(BeehaveNode.RUNNING)
@@ -54,7 +54,7 @@ func test_reset_when_child_finishes() -> void:
 
 
 func test_clear_running_child_after_run() -> void:
-	time_limiter.wait_time = 1.0
+	time_limiter.wait_time = 0.2
 	action.status = BeehaveNode.RUNNING
 	await runner.simulate_frames(1)
 	assert_that(time_limiter.running_child).is_equal(action)
