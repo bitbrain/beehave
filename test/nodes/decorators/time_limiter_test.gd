9e20844f2fe240619817bdc051b18320ee0df56a
bitbrain <miguel-gonzalez@gmx.de>
2023-11-19 17:20:19 +0000

    adjust timing once more

--- test/nodes/decorators/time_limiter_test.gd
+++ test/nodes/decorators/time_limiter_test.gd
@@ -54,10 +54,10 @@ func test_reset_when_child_finishes() -> void:
 
 
 func test_clear_running_child_after_run() -> void:
-	time_limiter.wait_time = 1.0
+	time_limiter.wait_time = 1.5
 	action.status = BeehaveNode.RUNNING
-	await runner.simulate_frames(1)
+	await runner.simulate_frames(1, 1)
 	assert_that(time_limiter.running_child).is_equal(action)
 	action.status = BeehaveNode.SUCCESS
-	await runner.simulate_frames(1, 1000)
+	await runner.simulate_frames(1, 1500)
 	assert_that(time_limiter.running_child).is_null()
