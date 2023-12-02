fe1c536993c274881fc98a5bf6659050d7acb33a
Brandon Chang <brandon@brandonchang.me>
2023-12-02 13:46:19 -0800

    uses get_menu_container in graphedit

--- addons/beehave/debug/graph_edit.gd
+++ addons/beehave/debug/graph_edit.gd
@@ -55,11 +55,7 @@ func _ready() -> void:
 	layout_button.flat = true
 	layout_button.focus_mode = Control.FOCUS_NONE
 	layout_button.pressed.connect(func(): horizontal_layout = not horizontal_layout)
-	# Godot 4.2+
-	if has_method("get_menu_hbox"):
-		call("get_menu_hbox").add_child(layout_button)
-	else:
-		call("get_zoom_hbox").add_child(layout_button)
+	get_menu_container().add_child(layout_button)
 	_update_layout_button()
 
 
