3c27fbc85830f0e52414ee39ecce995fdd2f9e67
Lucas Rafael <joaolucasrafael@gmail.com>
2023-12-03 14:03:22 +0000

    Add unit test.

--- test/beehave_tree_test.gd
+++ test/beehave_tree_test.gd
@@ -17,6 +17,26 @@ func test_normal_tick() -> void:
 	scene.beehave_tree._physics_process(1.0)
 	assert_that(scene.beehave_tree.status).is_equal(BeehaveNode.SUCCESS)
 
+func test_low_tick_rate() -> void:
+	var scene = create_scene()
+	scene_runner(scene)
+	scene.beehave_tree.tick_rate = 3
+	scene.beehave_tree._physics_process(1.0)
+	assert_that(scene.beehave_tree.status).is_equal(-1)
+	scene.beehave_tree._physics_process(1.0)
+	assert_that(scene.beehave_tree.status).is_equal(-1)
+	scene.beehave_tree._physics_process(1.0)
+	assert_that(scene.beehave_tree.status).is_equal(BeehaveNode.SUCCESS)
+
+func test_low_tick_rate_last_tick() -> void:
+	var scene = create_scene()
+	scene_runner(scene)
+	scene.beehave_tree.tick_rate = 3
+	scene.beehave_tree.last_tick = 1
+	scene.beehave_tree._physics_process(1.0)
+	assert_that(scene.beehave_tree.status).is_equal(-1)
+	scene.beehave_tree._physics_process(1.0)
+	assert_that(scene.beehave_tree.status).is_equal(BeehaveNode.SUCCESS)
 
 func test_nothing_running_before_first_tick() -> void:
 	var scene = create_scene()
