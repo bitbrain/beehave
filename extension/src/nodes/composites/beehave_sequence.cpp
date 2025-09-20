058096c0c55ad40b4d713fe5ab0b5e797b396363
LenNerd42 <16518527+LenNerd42@users.noreply.github.com>
2025-09-20 15:15:38 +0200

    Add BeehaveSequence C++ implementation (#414)
    * Implement BeehaveSequence
    
    * Fix incorrect file name in comment
    
    ---------
    
    Co-authored-by: LenNerd42 <16518527+TheCreator69@users.noreply.github.com>

---
+++ extension/src/nodes/composites/beehave_sequence.cpp
@@ -0,0 +1,70 @@
+/**************************************************************************/
+/*  beehave_sequence.cpp                                                  */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                               BEEHAVE                                  */
+/*                      https://bitbra.in/beehave                         */
+/**************************************************************************/
+/* Copyright (c) 2024-present Beehave Contributors.                       */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#include "beehave_sequence.h"
+
+using namespace godot;
+
+BeehaveSequence::BeehaveSequence() {
+
+}
+
+BeehaveSequence::~BeehaveSequence() {
+
+}
+
+void BeehaveSequence::_bind_methods() {
+
+}
+
+BeehaveTickStatus BeehaveSequence::tick(Ref<BeehaveContext> context) {
+    TypedArray<Node> children = get_children();
+    for (int i = 0; i < children.size(); ++i) {
+        if (i < successful_index) {
+            continue;
+        }
+        BeehaveTreeNode *child = cast_node(Object::cast_to<Node>(children[i]));
+        if (child == nullptr) {
+            // skip anything that is not a valid beehave node
+			continue;
+        }
+        BeehaveTickStatus response = child->tick(context);
+
+        switch(response) {
+            case SUCCESS:
+                ++successful_index;
+                break;
+            case FAILURE:
+                return FAILURE;
+            case RUNNING:
+                return RUNNING;
+        }
+    }
+    return BeehaveTickStatus::SUCCESS;
+}
